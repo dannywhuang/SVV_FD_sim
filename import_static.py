50244266d78c6fb05619983676f084ea59739b90
FVossen <61149685+FVossen@users.noreply.github.com>
2020-03-12 09:55:38 +0100

    -

--- import_static.py
+++ import_static.py
@@ -273,19 +273,21 @@ def staticThrust(inputFile, dataSet, standard=False):
     if standard == True:
         df = pd.read_csv('staticData/thrust_'+inputFile+'/'+dataSet+'/standard/thrust.dat',sep='\t')
         df.columns = ['Tpl','Tpr']
-        df['Tp'] = df['Tpl'].to_numpy() + df['Tpr'].to_numpy()
+        df['Tp']   = df['Tpl'].to_numpy() + df['Tpr'].to_numpy()
+        # df['Tcs']  = df['Tp']
     elif standard == False:
         df = pd.read_csv('staticData/thrust_'+inputFile+'/'+dataSet+'/thrust.dat',sep='\t')
         df.columns = ['Tpl','Tpr']
-        df['Tp'] = df['Tpl'].to_numpy() + df['Tpr'].to_numpy()
+        df['Tp']   = df['Tpl'].to_numpy() + df['Tpr'].to_numpy()
+        # df['Tc']   = df['Tp']
     return df
 
 
 ''' Create data files from the provided/measured data '''
 # excelToCSV('reference')
 # convertStaticToSI('reference')
-thrustToDAT('reference', SI=True, standard=False)
-thrustToDAT('reference', SI=True, standard=True)
+# thrustToDAT('reference', SI=True, standard=False)
+# thrustToDAT('reference', SI=True, standard=True)
 
 
 
